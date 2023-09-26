9b0cd84da6063f54e273f17775b8d1bca80bdaba
송규진 <songu9610@gmail.com>
2023-09-26 15:56:26 +0900

    [FEAT] : ⚙️: 변경
    id받음

--- app/src/main/java/com/example/android_mymedia/home/data/PlayListModel.kt
+++ app/src/main/java/com/example/android_mymedia/home/data/PlayListModel.kt
@@ -1,8 +1,8 @@
 package com.example.android_mymedia.home.data
 
 data class PlayListModel(
+    val id : String,
     val imgUrl: String,
     val title: String,
     val description: String,
-    val datetime: String
 )
