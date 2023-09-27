250c431840eb9176ea106182e8fc9a7059fb489c
송규진 <songu9610@gmail.com>
2023-09-27 14:45:05 +0900

    [FEAT] : ⚙️: 조회수 단위 자르는 메소드 추가

--- app/src/main/java/com/example/android_mymedia/unit/Unit.kt
+++ app/src/main/java/com/example/android_mymedia/unit/Unit.kt
@@ -5,4 +5,21 @@ import com.example.android_mymedia.BuildConfig
 object Unit {
     const val API = BuildConfig.YOUTUBE_API_KEY
     const val BASE_URL = "https://www.googleapis.com/youtube/v3/"
+
+    fun setViewCountFormat(count: String): String {
+        var resultFormat: String = ""
+
+        val countView = count.toLongOrNull()
+        if (countView != null) {
+            if (countView in 0..9999) {
+                resultFormat = (countView / 1000).toString() + "천"
+            } else if (countView in 10000..99999999) {
+                resultFormat = (countView / 10000).toString() + "만"
+            } else if (countView in 100000000..999999999999) {
+                resultFormat = (countView / 100000000).toString() + "억"
+            }
+        }
+
+        return resultFormat
+    }
 }
