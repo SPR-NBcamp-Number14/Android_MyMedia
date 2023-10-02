f7dee793a6d1589af3442864449b85dc7c65a933
hjdevelop <hyunjun6133@naver.com>
2023-10-02 15:23:54 +0900

    [FEAT] : ⚙️ Room 기능 완료

--- app/src/main/java/com/example/android_mymedia/detail/DetailActivity.kt
+++ app/src/main/java/com/example/android_mymedia/detail/DetailActivity.kt
@@ -4,32 +4,77 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.android_mymedia.R
 import com.example.android_mymedia.databinding.DetailActivityBinding
 import com.example.android_mymedia.home.data.PlayListModel
+import com.example.android_mymedia.room.VideoDatabase
+import com.example.android_mymedia.room.VideoEntity
 import com.example.android_mymedia.unit.Unit
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class DetailActivity : AppCompatActivity() {
 
-    private lateinit var binding : DetailActivityBinding
+    private lateinit var binding: DetailActivityBinding
 
     private var isLiked = false
 
     private val data: PlayListModel? by lazy {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             intent.getParcelableExtra(Unit.OBJECT_DATA, PlayListModel::class.java)
-        }
-        else {
+        } else {
             intent.getParcelableExtra<PlayListModel>(Unit.OBJECT_DATA)
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val db = VideoDatabase.getInstance(applicationContext)
+
         binding = DetailActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val videoId = db?.VideoDAO()?.getVideoById(data!!.id)
+
+            withContext(Dispatchers.Main) {
+                if (videoId!!.isNotEmpty()) {
+                    binding.detailBtnLike.setBackgroundResource(R.drawable.clicked_confirm_like_button)
+                    binding.detailBtnLike.setCompoundDrawablesWithIntrinsicBounds(
+                        0,
+                        0,
+                        R.drawable.ic_heart_filled_drawable,
+                        0
+                    )
+                    isLiked = true
+                } else {
+                    binding.detailBtnLike.setBackgroundResource(R.drawable.confirm_like_button)
+                    binding.detailBtnLike.setCompoundDrawablesWithIntrinsicBounds(
+                        0,
+                        0,
+                        R.drawable.ic_heart_drawable,
+                        0
+                    )
+                    isLiked = false
+                }
+            }
+        }
+
+        val videoInfo = VideoEntity(
+            data!!.id,
+            data!!.videoUrl,
+            data!!.highImgUrl,
+            data!!.title,
+            data!!.channelTitle,
+            data!!.description
+        )
+
+        Log.d("videoInfo", "${videoInfo}")
 
         binding.detailIbBackButton.setOnClickListener {
             finish()
@@ -45,17 +90,32 @@ class DetailActivity : AppCompatActivity() {
 
 
         binding.detailBtnLike.setOnClickListener {
-            if(isLiked) {
+            if (isLiked) {
                 binding.detailBtnLike.setBackgroundResource(R.drawable.confirm_like_button)
-                binding.detailBtnLike.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_heart_drawable, 0)
+                binding.detailBtnLike.setCompoundDrawablesWithIntrinsicBounds(
+                    0,
+                    0,
+                    R.drawable.ic_heart_drawable,
+                    0
+                )
                 Toast.makeText(this, "좋아요 리스트에서 삭제되었습니다.", Toast.LENGTH_LONG).show()
-            }
-            else {
+                CoroutineScope(Dispatchers.IO).launch {
+                    db!!.VideoDAO().deleteVideoById(data!!.id)
+                }
+            } else {
                 binding.detailBtnLike.setBackgroundResource(R.drawable.clicked_confirm_like_button)
-                binding.detailBtnLike.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_heart_filled_drawable, 0)
+                binding.detailBtnLike.setCompoundDrawablesWithIntrinsicBounds(
+                    0,
+                    0,
+                    R.drawable.ic_heart_filled_drawable,
+                    0
+                )
                 Toast.makeText(this, "좋아요 리스트에 추가되었습니다.", Toast.LENGTH_LONG).show()
+                CoroutineScope(Dispatchers.IO).launch {
+                    db!!.VideoDAO().insertVideo(videoInfo)
+                }
+                isLiked = !isLiked
             }
-            isLiked = !isLiked
         }
 
         binding.detailBtnShare.setOnClickListener {
@@ -63,7 +123,7 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
-    private fun shareUrl(url : String) {
+    private fun shareUrl(url: String) {
         val shareIntent = Intent().apply {
             action = Intent.ACTION_SEND
             putExtra(Intent.EXTRA_TEXT, url)
