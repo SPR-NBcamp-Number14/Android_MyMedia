1381e2241fa820f4f4a9b6706d9141ffc1234534
송규진 <songu9610@gmail.com>
2023-10-04 08:31:25 +0900

    [FEAT] : ⚙️: room data연결
    room과 myvideo연결.
    아직 동적으로 처리하지 않음.

--- app/src/main/java/com/example/android_mymedia/my_video/MyVideoViewModel.kt
+++ app/src/main/java/com/example/android_mymedia/my_video/MyVideoViewModel.kt
@@ -2,18 +2,33 @@ package com.example.android_mymedia.my_video
 
 import android.content.Context
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.example.android_mymedia.my_video.repository.MyRepository
 import com.example.android_mymedia.my_video.repository.MyRepositoryImpl
-import com.example.android_mymedia.room.VideoDatabase
 import com.example.android_mymedia.room.VideoEntity
-
+import kotlinx.coroutines.launch
 
 class MyVideoViewModel(
     private val repository: MyRepository
 ) : ViewModel() {
-    val liveBookMarkList: LiveData<List<VideoEntity>> get() = repository.getAllVideo()
+    private val _liveBookMarkList: MutableLiveData<List<VideoEntity>> = MutableLiveData()
+    val liveBookMarkList: LiveData<List<VideoEntity>> get() = _liveBookMarkList
+
+    init {
+        setLiveData()
+    }
+
+    fun setLiveData() {
+        viewModelScope.launch {
+            val videoList = repository.getLivedata()
+            val currentList = liveBookMarkList.value.orEmpty().toMutableList()
+            currentList.addAll(videoList)
+            _liveBookMarkList.value = currentList
+        }
+    }
 
 }
 
@@ -24,8 +39,7 @@ class MyVideoViewModelFactory(
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MyVideoViewModel::class.java)) {
             return MyVideoViewModel(
-                MyRepositoryImpl(
-                    VideoDatabase.getInstance(context))
+                MyRepositoryImpl(context)
             ) as T
         } else {
             throw IllegalArgumentException("Not found ViewModel class.")
