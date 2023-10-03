610792eaed6e5faa2d307dad0fb67857e291bab6
송규진 <songu9610@gmail.com>
2023-10-04 08:31:25 +0900

    [FEAT] : ⚙️: room data연결
    room과 myvideo연결.
    아직 동적으로 처리하지 않음.

--- app/src/main/java/com/example/android_mymedia/my_video/MyVideoViewModel.kt
+++ app/src/main/java/com/example/android_mymedia/my_video/MyVideoViewModel.kt
@@ -1,39 +1,48 @@
 package com.example.android_mymedia.my_video
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.android_mymedia.detail.DetailModel
-import com.example.android_mymedia.home.repository.HomeRepositoryImpl
-import com.example.android_mymedia.home.viewmdoel.HomeViewModel
-import com.example.android_mymedia.retrofit.RetrofitClient
+import androidx.lifecycle.viewModelScope
+import com.example.android_mymedia.my_video.repository.MyRepository
+import com.example.android_mymedia.my_video.repository.MyRepositoryImpl
+import com.example.android_mymedia.room.VideoEntity
+import kotlinx.coroutines.launch
 
 class MyVideoViewModel(
-
+    private val repository: MyRepository
 ) : ViewModel() {
-    private val _liveBookMarkList: MutableLiveData<List<DetailModel>> = MutableLiveData()
-    val liveBookMarkList: LiveData<List<DetailModel>> get() = _liveBookMarkList
+    private val _liveBookMarkList: MutableLiveData<List<VideoEntity>> = MutableLiveData()
+    val liveBookMarkList: LiveData<List<VideoEntity>> get() = _liveBookMarkList
 
     init {
-        _liveBookMarkList.value = mutableListOf<DetailModel>().apply {
+        setLiveData()
+    }
 
-            for (i in 0..4) {
-                add(
-                    DetailModel(
-                        id = "",
-                        videoUrl = "",
-                        mediumImgUrl = "",
-                        highImgUrl = "",
-                        title = "",
-                        channelTitle = "",
-                        description = "",
-                        isBooked = false
-                    )
-                )
-            }
+    fun setLiveData() {
+        viewModelScope.launch {
+            val videoList = repository.getLivedata()
+            val currentList = liveBookMarkList.value.orEmpty().toMutableList()
+            currentList.addAll(videoList)
+            _liveBookMarkList.value = currentList
         }
     }
 
+}
 
+class MyVideoViewModelFactory(
+    private val context: Context
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MyVideoViewModel::class.java)) {
+            return MyVideoViewModel(
+                MyRepositoryImpl(context)
+            ) as T
+        } else {
+            throw IllegalArgumentException("Not found ViewModel class.")
+        }
+    }
 }
