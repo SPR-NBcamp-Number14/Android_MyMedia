180193597ede89ea387dc61513ddb85b509fb8a8
송규진 <songu9610@gmail.com>
2023-10-05 14:14:23 +0900

    [FEAT] : ⚙️: 화면 이동 디테일 추가
    viewmodel 매소드 추가

--- app/src/main/java/com/example/android_mymedia/search/SearchAdapter.kt
+++ app/src/main/java/com/example/android_mymedia/search/SearchAdapter.kt
@@ -11,20 +11,31 @@ import com.example.android_mymedia.databinding.SearchItemListBinding
 import com.example.android_mymedia.search.searchdata.SearchListModel
 
 
-class SearchAdapter() : ListAdapter<SearchListModel, SearchAdapter.ViewHolder>(
+class SearchAdapter(
+    private val onItemClicked: (SearchListModel) -> Unit
+) : ListAdapter<SearchListModel, SearchAdapter.ViewHolder>(
     object : DiffUtil.ItemCallback<SearchListModel>() { //개념을 이해 해보면 좋을것
-    override fun areItemsTheSame(oldItem: SearchListModel, newItem: SearchListModel): Boolean {
-        return oldItem.id == newItem.id //나중에 id 로
-    }
+        override fun areItemsTheSame(oldItem: SearchListModel, newItem: SearchListModel): Boolean {
+            return oldItem.id == newItem.id //나중에 id 로
+        }
 
-    override fun areContentsTheSame(oldItem: SearchListModel, newItem: SearchListModel): Boolean {
-        return oldItem == newItem
-    }
+        override fun areContentsTheSame(
+            oldItem: SearchListModel,
+            newItem: SearchListModel
+        ): Boolean {
+            return oldItem == newItem
+        }
 
     }
 ) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(SearchItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return ViewHolder(
+            SearchItemListBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            ), onItemClicked
+        )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -32,15 +43,22 @@ class SearchAdapter() : ListAdapter<SearchListModel, SearchAdapter.ViewHolder>(
         holder.bind(item)
     }
 
-    class ViewHolder( val binding: SearchItemListBinding):RecyclerView.ViewHolder(binding.root){
+    class ViewHolder(
+        val binding: SearchItemListBinding,
+        private val onItemClicked: (SearchListModel) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
         private val context = binding.root.context
-        fun bind(item: SearchListModel) = with(binding){
+        fun bind(item: SearchListModel) = with(binding) {
             Glide.with(context)
                 .load(item.defaultImgUrl.toUri())
                 .into(searchImage)
 
             searchTitle.text = item.title
             searchId.text = item.description
+
+            itemView.setOnClickListener {
+                onItemClicked(item)
+            }
 
         }
     }
